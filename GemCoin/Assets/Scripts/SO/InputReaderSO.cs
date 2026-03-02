0612995a56f74ce9b98d93c6af800513e95d679e
강세환(플밍4기) <ghks5658@gmail.com>
2026-03-02 18:17:27 +0900

    [Feat] 인풋 관리를 위한 SO
    싱글톤 인풋 매니져에서 SO로 변경 계획
    인풋이 필요한 오브젝트에 직접 에셋을 달아주어
    매니저가 없는 씬에서도 독립적으로 작동하게 하기 위함
    (의존성 감소)

---
+++ GemCoin/Assets/Scripts/SO/InputReaderSO.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "InputReaderSO", menuName = "Scriptable Objects/InputReaderSO")]
+public class InputReaderSO : ScriptableObject, PlayerInputActions.IHubActions, PlayerInputActions.IRunningActions
+{
+    // 인풋 SO 에셋을 참조 중인 컴포넌트에서 구독 할 이벤트 목록
+    
+    // Hub 관련 이벤트
+    public event UnityAction<bool> LeftClickEvent = delegate { };
+    public event UnityAction CancelEvent = delegate { };
+    public event UnityAction<Vector2> ScrollWheelEvent = delegate { };
+    
+    // Running 관련 이벤트
+    public event UnityAction JumpEvent = delegate { };
+    public event UnityAction PauseEvent = delegate { };
+    public event UnityAction<bool> Skill1Event = delegate { };
+    public event UnityAction<bool> Skill2Event = delegate { };
+    public event UnityAction<bool> Skill3Event = delegate { };
+
+    
+
+    private PlayerInputActions _inputActions;
+    
+    public Vector2 PointerPosition { get; private set; }
+
+    private void OnEnable()
+    {
+        if (_inputActions == null)
+        {
+            _inputActions = new PlayerInputActions();
+            _inputActions.Running.SetCallbacks(this);
+            _inputActions.Hub.SetCallbacks(this);
+        }
+    }
+
+    // 맵 활성화 제어 (씬 컨트롤러에서 호출)
+    public void EnableRunningInput()
+    {
+        _inputActions.Hub.Disable();
+        _inputActions.Running.Enable();
+    }
+
+    public void EnableHubInput()
+    {
+        _inputActions.Running.Disable();
+        _inputActions.Hub.Enable();
+    }
+    //--------------Hub-----------------------------------------------
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed) LeftClickEvent?.Invoke(true);
+        else if (context.canceled) LeftClickEvent?.Invoke(false);
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        PointerPosition = context.ReadValue<Vector2>();
+    }
+
+    public void OnScrollWheel(InputAction.CallbackContext context)
+    {
+        if (context.performed) 
+        {
+            ScrollWheelEvent.Invoke(context.ReadValue<Vector2>());
+        }
+    }
+
+    public void OnCancel(InputAction.CallbackContext context)
+    {
+        if (context.performed) CancelEvent?.Invoke();
+    }
+    //--------------Running-----------------------------------------------
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed) JumpEvent?.Invoke();
+    }
+
+    public void OnSkill1(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Skill1Event.Invoke(true); // 키 다운 (True)
+        }
+        else if (context.canceled)
+        {
+            Skill1Event.Invoke(false); // 키 업 (False)
+        }
+    }
+
+    public void OnSkill2(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Skill2Event.Invoke(true); // 키 다운 (True)
+        }
+        else if (context.canceled)
+        {
+            Skill2Event.Invoke(false); // 키 업 (False)
+        }
+    }
+
+    public void OnSkill3(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Skill3Event.Invoke(true); // 키 다운 (True)
+        }
+        else if (context.canceled)
+        {
+            Skill3Event.Invoke(false); // 키 업 (False)
+        }
+    }
+
+    public void OnPause(InputAction.CallbackContext context)
+    {
+        if (context.performed) PauseEvent?.Invoke();
+    }
+}
