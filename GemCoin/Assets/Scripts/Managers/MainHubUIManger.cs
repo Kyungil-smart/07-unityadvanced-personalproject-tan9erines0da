2c99d606eaa13ba48cbdb8a8f721568aa546ea02
강세환(플밍4기) <ghks5658@gmail.com>
2026-03-04 13:46:18 +0900

    [Feat]메인 씬 UI레이어 배치

---
+++ GemCoin/Assets/Scripts/Managers/MainHubUIManger.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Behavior;
+using UnityEngine;
+public class MainHubUIManger : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private BehaviorGraphAgent _agent; 
+    [SerializeField] private GameObject[] _layers;
+
+    // 버튼에서 호출할 함수
+    public void OnClickChangeState(int newStateIndex)
+    {
+        _agent.SetVariableValue("TargetState", (HubUIState)newStateIndex);
+    }
+
+    // BT의 Action 노드에서 호출할 실제 로직
+    public void SetLayerActive(HubUIState state)
+    {
+        for (int i = 0; i < _layers.Length; i++)
+        {
+            // 0=Title, 1=Character, 2=Way, 3=Event, 4=Shop
+            // UI를 장면 단위로 구분하고 장면에 맞는 UI그룹만 활성
+            // 목표하는 레이어 제외 비활성화, 목표레이어만 활성
+            _layers[i].SetActive(i == (int)state);
+        }
+    }
+}
---
+++ GemCoin/Assets/Scripts/Managers/TransitionManger.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class TransitionManager : SingletonMono<TransitionManager>
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _transitionDelay = 0.2f;
+    
+
+    public void PlayTransition(Action onMidPoint)
+    {
+        StartCoroutine(TransitionRoutine(onMidPoint));
+    }
+
+    private IEnumerator TransitionRoutine(Action onMidPoint)
+    {
+        _animator.SetTrigger("In");
+
+        yield return new WaitForSeconds(_transitionDelay);
+
+        // 암전 시점에 레이어 교체/씬 로드 로직 실행
+        onMidPoint?.Invoke();
+
+        _animator.SetTrigger("Out");
+    }
+}
